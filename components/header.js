4b7f768c914f1f05601581b07b33c548a3e78d82
Romulo Banares <romulo.banares.contractor@bbva.com>
2021-02-26 11:15:00 +0100

    modify header component

--- components/header.js
+++ components/header.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { Flex, Heading } from "@chakra-ui/react";
 
 const Header = () => (
-    <Flex as="nav" align="center" mr={5}>
-        <Heading as="h1" size="lg" letterSpacing={'-.1rem'}>
-            Video Processing App
-        </Heading>
+    <Flex as="nav" align="center" justify="space-between" wrap="wrap" padding="1.5rem" bg="teal.500" color="white" marginBottom="2rem">
+        <Flex align="center" mr={5}>
+            <Heading as="h1" size="lg" letterSpacing={'-.1rem'}>
+                Video Processing App
+            </Heading>
+        </Flex>
     </Flex>
 )
 
